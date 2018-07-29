49dbf15c939d69a53963d09345d844d7ab593d89
Haktan Yucesoy <38834423+Hacktin@users.noreply.github.com>
2018-07-29 16:48:27 +0300

    Depency Injection with Ninjepect(Layer.Business)
    Ninjepect Framework

---
+++ Depency Module/Depency_Module.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Layer.Business.Abstract;
+using Layer.Business.Concart;
+using Layer.Data.Abstract;
+using Layer.Data.Concart.EntityFramework;
+using Ninject.Modules;
+
+namespace Layer.Business.Depency_Module
+{
+    public class Depency_Module : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IStudentLogic>().To<StudentLogic>().InSingletonScope();
+            Bind<IStudentClubLogic>().To<StudentClubLogic>().InSingletonScope();
+            Bind<IStudentScoreLogic>().To<StudentScoreLogic>().InSingletonScope();
+
+            Bind<IStudentDAL>().To<EntityFrameworkStudent>().InSingletonScope();
+            Bind<IStudent_Club_DAL>().To<EntityFrameworkStudentClub>().InSingletonScope();
+            Bind<IStudent_Score_DAL>().To<EntityFrameworkStudentScore>().InSingletonScope();
+
+        }
+    }
+}
---
+++ Depency Module/InstanceFactory.cs
@@ -0,0 +1,19 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layer.Business.Depency_Module
+{
+  public class InstanceFactory
+    {
+        public static T GetInstance<T>()
+        {
+            var kernel = new StandardKernel(new Depency_Module());
+
+            return kernel.Get<T>();
+        }
+    }
+}
