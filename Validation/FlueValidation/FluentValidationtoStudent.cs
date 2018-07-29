a1cd3270a3854dfad4d6316e609ada23e710e4d8
Haktan Yucesoy <38834423+Hacktin@users.noreply.github.com>
2018-07-29 16:49:53 +0300

    Fluent Validation(Layer.Business)
    Fluent Validation Framework

---
+++ Validation/FlueValidation/FluentValidationtoStudent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Layer.Entity.Concart;
+
+namespace Layer.Business.Validation.FlueValidation
+{
+   public class FluentValidationtoStudent:AbstractValidator<Student>
+    {
+        public FluentValidationtoStudent()
+        {
+            RuleFor(p => p.Name).NotEmpty().WithMessage("İsim boş bırakılamaz");
+            RuleFor(p => p.Surname).NotEmpty().WithMessage("Soyad boş bırakılamaz");
+
+            RuleFor(p => p.ClubID).NotEmpty().WithMessage("Kulüp ID'sı boş bırakılamaz");
+
+
+        }
+        
+    }
+}
