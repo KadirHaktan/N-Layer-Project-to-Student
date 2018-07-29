ae7f56d2756e4f6396d1ca87b37e0835f2127e44
Haktan Yucesoy <38834423+Hacktin@users.noreply.github.com>
2018-07-29 16:51:00 +0300

    Tools for Fluent Validation

---
+++ Tools/ValidationTools/Fluent Validation Tool/FValidationToStudent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Resources;
+
+namespace Layer.Business.Tools.ValidationTools.Fluent_Validation_Tool
+{
+    class FValidationToStudent
+    {
+        public static void InstenceValidation(IValidator validator, object entity)
+        {
+            var result = validator.Validate(entity);
+
+
+            if (result.Errors.Count > 0)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
